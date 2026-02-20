0fa19fcb9d75199f6f5b310bb69486d7c873a3db
Jovanus Irwan <jovanusirwan@gmail.com>
2026-02-20 17:10:35 +0700

    chore: initial setup for catalogue service

---
+++ src/main/java/id/ac/ui/cs/advprog/bidmartcatalogueservice/BidmartCatalogueServiceApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.bidmartcatalogueservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BidmartCatalogueServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BidmartCatalogueServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/ac/ui/cs/advprog/bidmartcatalogueservice/BidmartCatalogueServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.bidmartcatalogueservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BidmartCatalogueServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
